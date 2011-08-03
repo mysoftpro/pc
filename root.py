63e497970c1df60d52c111e1d448645229c37220
Aleksey Ganzha <aganzha@yandex.ru>
2011-08-03 11:49:28 +0300

    store catalog as separate docs

--- root.py
+++ root.py
@@ -144,8 +144,6 @@ class Root(Resource):
             if len(self.cookies) < 10:
                 reactor.callLater(0, self.collectCookies)
         u = str(request.URLPath())
-        print "-------------------------------------"
-        print u
         if ('http://' + self.host_url + '/' == u):
             return self.static.getChild(name, request)
         return self
@@ -154,7 +152,11 @@ from pc.secure import xml_source,xml_method, xml_login, xml_password
 from lxml import etree
 
 class XmlGetter(Resource):
-    def pr(self, res):
+    def pr(self, fail):
+        log.startLogging(sys.stdout)
+        log.msg(fail)
+
+    def storeWholeCatalog(self, res):
         log.startLogging(sys.stdout)
         src = base64.decodestring(res)
         sio = StringIO(src)
@@ -163,16 +165,29 @@ class XmlGetter(Resource):
         tree = etree.parse(gz)
         root = tree.getroot()    
         jsroot = xmlToJson(toDict(root.attrib), root)
-        # doc = xmlToJson(gz.read())
         couch.saveDoc(jsroot, docId="catalog")
-        # f = open(os.path.join(os.path.dirname(__file__), str(datetime.now()).replace(" ","_").replace(":","-")) + ".xml", 'w')
-        # f.write(gz.read())
-        # f.close()
-        
+    
+    def storeItem(self, res, gen):
+        try:
+            item = gen.next()
+        except StopIteration:
+            return
+        d = couch.saveDoc(item, docId=item.pop('code'))
+        d.addCallback(self.storeItem, gen)
+ 
+    def storeDocs(self, res):
+        log.startLogging(sys.stdout)
+        src = base64.decodestring(res)
+        sio = StringIO(src)
+        gz = gzip.GzipFile(fileobj=sio)
+        tree = etree.parse(gz)
+        root = tree.getroot()
+        gen = xmlToDocs([toDict(root.attrib)], root)
+        self.storeItem(None, gen)
         
     def render_GET(self, request):
         proxy = Proxy(xml_source)
-        proxy.callRemote(xml_method, xml_login, xml_password).addCallbacks(self.pr, self.pr)
+        proxy.callRemote(xml_method, xml_login, xml_password).addCallbacks(self.storeDocs, self.pr)
         return "ok"
 
 def toDict(attr):
@@ -180,6 +195,23 @@ def toDict(attr):
     for k,v in attr.iteritems():
         res.update({k:v})
     return res
+
+
+
+def xmlToDocs(catalog_obs, catalog):
+    for el in catalog:
+        if el.tag == 'catalog':            
+            new_catalog_obs = [c for c in catalog_obs]
+            new_catalog_obs.append(toDict(el.attrib))
+            gen = xmlToDocs(new_catalog_obs, el)
+            for item in gen:
+                yield item
+        elif el.tag == 'item':
+            item = toDict(el.attrib)            
+            item.update({'catalogs':catalog_obs})
+            item.update({'text':el.text})
+            yield item
+            
 
 def xmlToJson(catalog_ob, catalog):
     catalog_ob.update({'xmltype':catalog.tag})
@@ -198,13 +230,13 @@ def xmlToJson(catalog_ob, catalog):
     return catalog_ob
 
 
-def parse():
-    f = open('c:\\Users\\agn\\pc\\2011-08-02_14-00-50.126000.xml')
-    tree = etree.parse(f)
-    root = tree.getroot()    
-    jsroot = xmlToJson(toDict(root.attrib), root)
-    f.close()
-    f = open('c:\\Users\\agn\\pc\\2011-08-02_14-00-50.126000.json', 'w')
-    f.write(simplejson.dumps(jsroot))
-    f.close()
+# def parse():
+#     f = open('c:\\Users\\agn\\pc\\2011-08-02_14-00-50.126000.xml')
+#     tree = etree.parse(f)
+#     root = tree.getroot()    
+#     jsroot = xmlToJson(toDict(root.attrib), root)
+#     f.close()
+#     f = open('c:\\Users\\agn\\pc\\2011-08-02_14-00-50.126000.json', 'w')
+#     f.write(simplejson.dumps(jsroot))
+#     f.close()
     
