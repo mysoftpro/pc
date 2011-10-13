44b3ac85d89c0ac4395cd2aa90f033eaf41edda3
aganzha <aganzha@aganzha-asus>
2011-10-13 13:25:34 +0300

    texts

--- static/how.js
+++ static/how.js
@@ -4,7 +4,7 @@ $(function(){
                               description: "Список компонентов этой модели с ценами. <br/>Чтобы прочитать описания компонента нужно кликнуть его название.<br/> Внизу <span style=\"float:none;margin:0;\" class=\"guiderdiv\">кнопки</span>, которыми можно подвигать окошко.",
                               id: "howguider",
                               position: 3,
-                              width: 600
+                              width: 500
                           }).show();
       $('#hselect').chosen();
       $('.modellink').attr('target','_blank');
