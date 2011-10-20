9ee16e0fe46325f6708a7973a9b0277f96c1e23a
aganzha <aganzha@aganzha-asus>
2011-10-20 12:30:11 +0300

    warranty and the bill done

--- realm/warranty.js
+++ realm/warranty.js
@@ -1,3 +1,8 @@
+_.templateSettings = {
+    interpolate : /\{\{(.+?)\}\}/g
+    ,evaluate: /\[\[(.+?)\]\]/g
+};
+
 $(function(e){
       var splitted = document.location.search.split('=');
       if (splitted.length!=2 || splitted[0]!='?id')
@@ -18,26 +23,47 @@ $(function(e){
                          alert('Что-то пошло не так!');
                          return;
                      }
-		     var table = $('#components');
+                     var table = $('#components');
+		     var tottal = 0;
                      for (var i=0;i<data['items'].length;i++){
                          var tr = $(document.createElement('tr'));
-			 var no = $(document.createElement('td'));
-			 no.text(i);
-			 var name = $(document.createElement('td'));
-			 name.text(data['items'][i]['name']);
-			 var count = $(document.createElement('td'));
-			 count.text(data['items'][i]['pcs']);
-			 var fac = $(document.createElement('td'));
-			 fac.text(data['items'][i]['factory']);
-			 var war = $(document.createElement('td'));
-			 war.text(data['items'][i]['warranty']);
+                         var no = $(document.createElement('td'));
+                         no.css('text-align','center');
+                         no.text(i+1);
+                         var name = $(document.createElement('td'));
+                         name.html(data['items'][i]['name']);
+                         var count = $(document.createElement('td'));
+                         count.css('text-align','center');
+                         count.text(data['items'][i]['pcs']);
+
+                         var fac = $(document.createElement('td'));
+
+                         var war = $(document.createElement('td'));                         
+			 if (document.location.href.match('warranty')){
+                             fac.text(data['items'][i]['factory']);
+                             war.text(data['items'][i]['warranty']);
+                         }
+                         else{
+			     var price = data['items'][i]['price'];
+			     var pcs =data['items'][i]['pcs']; 
+                             fac.text(price);
+                             war.text(price*pcs);
+			     tottal += price*pcs;
+                         }
 			 tr.append(no);
-			 tr.append(name);
-			 tr.append(count);
-			 tr.append(fac);
-			 tr.append(war);
-			 table.append(tr);			 
+                         tr.append(name);
+                         tr.append(count);
+                         tr.append(fac);
+                         tr.append(war);                         
+                         table.append(tr);
                      }
+		     if (tottal>0){	
+			 table.append(_.template('<tr><td colspan="5" class="total">Итого: {{tottal}} р.</td></tr>',
+						 {tottal:tottal}));
+			 var b = $('body');
+			 b.append('<div style="margin-top:300px;">&nbsp;</div>');
+			 b.append(b.html());
+		     }
                  }
              });
 
