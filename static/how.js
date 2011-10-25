2b11fd95e63f0411538a741c921cc515509811f2
Ganzha Aleksey <aganzha@yandex.ru>
2011-10-25 15:41:52 -0400

    styles

--- static/how.js
+++ static/how.js
@@ -1,7 +1,7 @@
 head.ready(function(){
       guider.createGuider({
                               attachTo: "#howguider",
-                              description: "Список компонентов этой модели с ценами. <br/>Чтобы прочитать описания компонента, нужно кликнуть его название.<br/> Внизу - <span style=\"float:none;margin:0;\" class=\"guiderdiv\">кнопки</span>, которыми можно подвигать окошко.",
+                              description: "Список компонентов этой модели с ценами. <br/>Чтобы прочитать описание компонента, нужно кликнуть его название.<br/> Внизу - <span style=\"float:none;margin:0;\" class=\"guiderdiv\">кнопки</span>, которыми можно подвигать окошко.",
                               id: "howguider",
                               position: 3,
                               width: 500
