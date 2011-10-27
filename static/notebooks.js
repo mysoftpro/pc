1548be77b082a6a0d404f629ee36ecfd31ced647
aganzha <aganzha@aganzha-asus>
2011-10-27 11:58:57 +0300

    notes arrows

--- static/notebooks.js
+++ static/notebooks.js
@@ -28,14 +28,65 @@ function sortByClick(el, fu){
                               sortNotes(el,fu);
                               el.find('.asc').attr('class','asc asca');
                               el.find('.desca').attr('class','desc');
+			      left_halfes= $('#s_price .nleft.half_vertical');
+			      right_halfes= $('#s_price .nright.half_vertical');
+			      if(notes_active)
+				  $(notes_active.first()).click();
                           });
     el.find('.desc').click(function(e){
                                sortNotes(el,reversed);
                                el.find('.asca').attr('class','asc');
                                el.find('.desc').attr('class','desc desca');
+			       left_halfes= $('#s_price .nleft.half_vertical');
+			       right_halfes= $('#s_price .nright.half_vertical');
+			       if(notes_active)				   
+				   $(notes_active.first()).click();
                            });
 }
 var notes_active;
+var left_halfes;
+var right_halfes;
+var halfes_offset = -4;
+function showArrows(){
+    var left,mid,right;
+    var ob = {};
+    if (!left_halfes)
+        left_halfes= $('#s_price .nleft.half_vertical');
+    if (!right_halfes)
+        right_halfes= $('#s_price .nright.half_vertical');
+    left_halfes.hide();
+    right_halfes.hide();
+    for (var i=0;i<notes_active.length;i++){
+        var div = $(notes_active.get(i));
+        var parent = div.parent();
+        var fill = function(parent_id, token){
+            if (parent.attr('id') !==parent_id)
+                return;
+            ob[token] = {'div':div};
+            ob[token]['index'] = parent.children().index(div);
+        };
+        fill('s_performance', 'left');
+        fill('s_price', 'mid');
+        fill('s_size', 'right');
+    }
+    if (ob['mid']['index']<ob['left']['index']){
+        for (var i=ob['mid']['index'];i<ob['left']['index'];i++)
+            $(left_halfes.get(i+halfes_offset)).show();
+        }
+    else{
+        for (var i=ob['left']['index'];i<ob['mid']['index'];i++)
+            $(left_halfes.get(i+halfes_offset)).show();
+    }
+    if (ob['mid']['index']<ob['right']['index']){
+        for (var i=ob['mid']['index'];i<ob['right']['index'];i++)
+            $(right_halfes.get(i+halfes_offset)).show();
+        }
+    else{
+        for (var i=ob['right']['index'];i<ob['mid']['index'];i++)
+            $(right_halfes.get(i+halfes_offset)).show();
+    }    
+}
+
 
 head.ready(function(){
 
@@ -111,39 +162,40 @@ head.ready(function(){
                                     }
                                     notebook_to_cart.find('h2').remove();
                                     var dos = '';
-				    if (doc['text'].toLowerCase().match('/dos'))
-					dos = ' ВНИМАНИЕ! НЕТ ОПЕРАЦИОННОЙ СИСТЕМЫ!';
-				    notebook_to_cart.append('<h2>' + doc['text']+
-							    ' '+doc['price']+' р.'+dos+'</h2>');
-				    
+                                    if (doc['text'].toLowerCase().match('/dos'))
+                                        dos = ' ВНИМАНИЕ! НЕТ ОПЕРАЦИОННОЙ СИСТЕМЫ!';
+                                    notebook_to_cart.append('<h2>' + doc['text']+
+                                                            ' '+doc['price']+' р.'+dos+'</h2>');
+                                    showArrows();
+
                                 });
-	       $('#tocart').click(function(e){		
-				       $.ajax({
-						  url:'savenote',
-						  data:{id:getNoteClass(notes_active)},
-						  success:function(data){
-						      if (data =='fail'){
-							  alert('Что-то пошло не так :(');
-							  return;
-						      }
-							  
-						      var cart_el = $('#cart');
-						      if (cart_el.length>0){
-							  cart_el.text('Корзина('+
-								       $.cookie('pc_cart')
-								       +')');
-						      }
-						      else{
-							  $('#main_menu')
-							      .append(_.
-								      template('<li><a id="cart" href="/cart/{{cart}}">Корзина(1)</a></li>',
-									       {
-												cart:$.cookie('pc_user')
-											    }));
-						      }
-						      alert('Получилось!');
-						  }
-					      });
-				   });
+               $('#tocart').click(function(e){
+                                       $.ajax({
+                                                  url:'savenote',
+                                                  data:{id:getNoteClass(notes_active)},
+                                                  success:function(data){
+                                                      if (data =='fail'){
+                                                          alert('Что-то пошло не так :(');
+                                                          return;
+                                                      }
+
+                                                      var cart_el = $('#cart');
+                                                      if (cart_el.length>0){
+                                                          cart_el.text('Корзина('+
+                                                                       $.cookie('pc_cart')
+                                                                       +')');
+                                                      }
+                                                      else{
+                                                          $('#main_menu')
+                                                              .append(_.
+                                                                      template('<li><a id="cart" href="/cart/{{cart}}">Корзина(1)</a></li>',
+                                                                               {
+                                                                                                cart:$.cookie('pc_user')
+                                                                                            }));
+                                                      }
+                                                      alert('Получилось!');
+                                                  }
+                                              });
+                                   });
                $('.nname').first().click();
            });
