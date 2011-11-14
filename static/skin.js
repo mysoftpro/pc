b032f0b009c879cde78b53b1a3f786abc15200fd
aganzha <aganzha@aganzha-asus>
2011-11-14 17:12:42 +0300

    expandable menu

--- static/skin.js
+++ static/skin.js
@@ -1,31 +1,36 @@
+_.templateSettings = {
+    interpolate : /\{\{(.+?)\}\}/g
+    ,evaluate: /\[\[(.+?)\]\]/g
+};
+
 function CartAndContacts(){
     var cart = $.cookie('pc_cart');
     var faq = $('#faqli a');
     if (cart){
-	var link = $.cookie('pc_user');
-	if (document.location.href.match('skin')){
-	    var splitted = document.location.search.split('?')[1];
-	    var pairs = splitted.split('&');
-	    for (var i=0;i<pairs.length;i++){
-		var spli = pairs[i].split('=');
-		if (spli[0]=='skin'){
-		    link+='?skin='+spli[1];
-		    break;
-		}
-	    }
-	}
-	$('#main_menu')
-	    .append('<li style="width:0;"><a id="cart" href="/cart/' +link+
-		    '">Корзина('+cart+')</a></li>');
-	$('#cart').parent().animate({'width':'94px'},400);
+        var link = $.cookie('pc_user');
+        if (document.location.href.match('skin')){
+            var splitted = document.location.search.split('?')[1];
+            var pairs = splitted.split('&');
+            for (var i=0;i<pairs.length;i++){
+                var spli = pairs[i].split('=');
+                if (spli[0]=='skin'){
+                    link+='?skin='+spli[1];
+                    break;
+                }
+            }
+        }
+        $('#main_menu')
+            .append('<li style="width:0;"><a id="cart" href="/cart/' +link+
+                    '">Корзина('+cart+')</a></li>');
+        $('#cart').parent().animate({'width':'94px'},400);
     }
     if (!document.location.href.match('faq'))
-	_.delay(function(){
-		    faq.animate({opacity:'1'},300);
-		}, 800);
+        _.delay(function(){
+                    faq.animate({opacity:'1'},300);
+                }, 800);
 
     function swapPhone(text){
-	return function(e){$('#phone').html(text);};
+        return function(e){$('#phone').html(text);};
     }
     $('#beeline').click(swapPhone('8 (909) 792-22-39'));
     $('#mts').click(swapPhone('8 (911) 462-42-52'));
@@ -33,37 +38,82 @@ function CartAndContacts(){
     $('#skype').click(swapPhone('<a href="skype:buildpc.ru">buildpc.ru</a>'));
 }
 
+
+var expands = {'/howtochoose':{'urls':[
+                                   {url:'/howtouse',title:"Как пользоваться сайтом"},
+                                   {url:'/howtochoose',title:'Как выбирать компьютер'},
+                                   {url:'/processor',title:'Как выбирать процессор'},
+                                   {url:'/motherboard',title:'Как выбирать материнскую плату'},
+                                   {url:'/video',title:'Как выбирать видеокарту'}
+                               ],
+                               'picture':'binocular.png',
+                               'lock':undefined
+                              }};
+
+var etempate = _.template('<div><a href={{url}}>{{title}}</a></div>');
+
+function expandMenu(link){
+    var div = $(document.createElement('div'));
+    div.html('');
+    div.attr('class','expanded');
+    var expas = expands[link.attr('href').split('?')[0]];
+    _(expas['urls'])
+        .each(function(el){
+                  div.append(etempate(el));
+              });
+    div.attr('id',link.attr('id')+'expandable');
+    link.after(div);
+    div.animate({'opacity':'1.0'},400);
+
+    function hideexpa(delta){
+        _.delay(function(e){
+                    if (expas['lock'])return;
+                    div.animate({'opacity':'0.0'},400,function(){div.remove();});
+            }, delta);
+    }
+
+    div.mouseenter(function(e){
+                       expas['lock']=true;
+                   });
+    div.mouseleave(function(e){
+                        expas['lock']=false;
+                        hideexpa(300);
+                   });
+    hideexpa(3000);
+
+}
 head.ready(function(){
-	       	       	       
-	       if ($.browser.opera){
-	       	   $('html').css('height','auto');
-	       }
-	       
-	       CartAndContacts();
 
-	       $('.expandable').click(function(e){
-					  console.log(e.target);
-				      });
+               if ($.browser.opera){
+                   $('html').css('height','auto');
+               }
 
+               CartAndContacts();
 
+               $('.expandable').click(function(e){
+                                          expandMenu($(e.target).next());
+                                      });
+               $('.expandable').next().click(function(e){
+                                                 e.preventDefault();
+                                                 expandMenu($(e.target));
+                                      });
 
+               var astro = $('#astro');
+               var autumn = $('#autumn');
 
-	       var astro = $('#astro');	       
-	       var autumn = $('#autumn');	       
-
-	       var splitted = document.location.search.split('?')[1];
-	       if (splitted==undefined){
-		   $('#autumn').attr('href','?'+'skin=home');
-		   return;
-	       }
-	       var pairs = splitted.split('&');
-	       var pairs_filtered = [];
-	       for (var i=0;i<pairs.length;i++){
-		   var spli = pairs[i].split('=');
-		   if (spli[0]=='skin')continue;
-		   pairs_filtered.push(pairs[i]);
-	       }
-	       var joined = pairs_filtered.join('&');
-	       astro.attr('href','?'+joined);
-	       autumn.attr('href','?'+joined+'&skin=home');
-	   });
+               var splitted = document.location.search.split('?')[1];
+               if (splitted==undefined){
+                   $('#autumn').attr('href','?'+'skin=home');
+                   return;
+               }
+               var pairs = splitted.split('&');
+               var pairs_filtered = [];
+               for (var i=0;i<pairs.length;i++){
+                   var spli = pairs[i].split('=');
+                   if (spli[0]=='skin')continue;
+                   pairs_filtered.push(pairs[i]);
+               }
+               var joined = pairs_filtered.join('&');
+               astro.attr('href','?'+joined);
+               autumn.attr('href','?'+joined+'&skin=home');
+           });
