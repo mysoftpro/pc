4f603a7d92bbe011f47239381bd8ba3ef993926f
aganzha <aganzha@aganzha-asus>
2011-12-05 13:06:05 +0300

    my stock

--- json/catalogs.map.js
+++ json/catalogs.map.js
@@ -1,5 +1,7 @@
 function(doc){
-    if (doc.catalogs && doc.price && doc.price>0 && doc.stock1 && doc.stock1>0){
+    var catalog_item = doc.catalogs && doc.price && doc.price>0;
+    var in_stock = (doc.stock1 && doc.stock1>0) || (doc.mystock && doc.mystock>0);
+    if (catalog_item && in_stock){
 	var to_emit	 = [];
 	for(var i=0;i<doc.catalogs.length;i++){
 	    to_emit.push(doc.catalogs[i]['id']);   
