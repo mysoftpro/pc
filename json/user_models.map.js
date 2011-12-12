082a3586986446dd7f9abf1b9e4a8bd886774ed5
aganzha <aganzha@aganzha-asus>
2011-12-12 21:09:21 +0300

    styles

--- json/user_models.map.js
+++ json/user_models.map.js
@@ -1,5 +1,15 @@
 function(doc){
     if (doc['building']&&doc['author']){
-	emit(doc['author'],doc['date']);
+	var price = 0;
+	if (doc.items['7396'])
+	    emit(1);
+	// for (var key in doc['original_prices']){
+	//     var p =doc['original_prices'][key];
+	//     //windows
+	//     if (key==='17398' || key==='14439' || key==='19843' || key==='17571' || key==='17400')
+	// 	p = p/31.5;
+	//     price+=p;
+	// }
+	// emit(doc['author'],Math.round(price*31.5/1000));
     }
 }
