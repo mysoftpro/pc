08c2e6e27af6fd0d0259ac8b93276388186d5b8d
aganzha <aganzha@aganzha-asus>
2012-01-22 17:08:16 +0300

    oke

--- static/credit.js
+++ static/credit.js
@@ -1,23 +1,41 @@
 function recalculate(e){
-    var tot = $('#total');var t;
+    var tot = $('#total');var total;
     if (tot[0].tagName.toLowerCase()=='input')
-	t = parseInt($('#total').val());
+	total = parseInt($('#total').val());
     else
-	t = parseInt($('#total').text());
+	total = parseInt($('#total').text());
     var m = parseInt($('#monthes').val());
     var k;
-    var c = parseInt($('#cache').val());
-    if (!c)c = 0;
-    if (c >= t/5){
+    var cache = parseInt($('#cache').val());
+    if (!cache)cache = 0;
+    if (cache >= total/5){
 	k= monthly['20'][m];
     }
-    else if(c >= t/10){
+    else if(cache >= total/10){
 	 k= monthly['10'][m];
     }
     else{
 	k= monthly['0'][m];
     }
-    $('#monthly').text(parseInt((t-c)*k*100+0.01)/100).parent().show();
+    var monthly_pay = parseInt((total-cache)*k*100+0.01)/100;
+    $('#monthly').text(monthly_pay).parent().show();
+    $('input[name="initPay"]').val(cache);
+    $('input[name="creditAmount"]').val(total-cache);
+    $('input[name="price"]').val(total);
+    $('input[name="numInstalment"]').val(m);
+    $('input[name="instalment"]').val(monthly_pay);
+    var d = new Date();
+    d.setDate(d.getDate()+20);
+    var year = d.getFullYear();
+    var month = (d.getMonth()+1).toString();
+    if (month.length==1)
+	month = "0"+month;
+    var day = d.getDate().toString();
+    if (day.length==1)
+	day = "0"+day;
+    $('input[name="annuityDate"]').val(day+'.'+month+'.'+year);
+    $('input[name="annuityDay"]').val(day);
+    
 }
 function init(){
     $('#calc').click(recalculate);
