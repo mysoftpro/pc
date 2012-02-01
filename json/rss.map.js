591c238bd5db9d908f4b7e5471dc95190d3012f5
aganzha <aganzha@aganzha-asus>
2012-02-01 14:31:15 +0300

    full posts in rss

--- json/rss.map.js
+++ json/rss.map.js
@@ -2,7 +2,7 @@ function(doc){
     if (doc['type'] && doc['type']=='blog' && doc['title']){
         emit(doc['_id'],
 	     {'title':doc['title'],
-	      'desc':doc['txt'].substring(0,100)
+	      'desc':doc['txt']
 	      .replace(/&/g, "&amp;")
 	      .replace(/>/g, "&gt;")
 	      .replace(/</g, "&lt;").replace(/"/g, "&quot;"),
