e69163ecef9bbfa3f3f4f6d642307c972a204f67
aganzha <aganzha@aganzha-asus>
2012-02-15 08:30:48 +0300

    remove popup promo

--- static/guides.js
+++ static/guides.js
@@ -613,34 +613,6 @@ function renameUserModel(){
 }
 renameUserModel();
 
-function showPromo(){
-    var toppopup = $('#toppopup');
-
-    if (!(document.location.href.split('?')[0].split('/').pop() ==='computer')
-        || $.cookie('pc_promo_shown'))return;
-    toppopup.html('<p>Посмотрите наше <a target="_blank" href="/promotion/ajax">Спец предложение</a><br/> Полностью укомплектованный суперсовременный компьютер с установленным Windows, в качестве мультимедийной станции и для игр. <div style="font-size:12px;margin-top:-10px;"> Спецпредложения в пункте меню "Еще"</div></p><div class="small_square_button small_reset" style="margin-top: -45px;margin-right: 140px;">Закрыть</div>');
-    _.delay(function(){
-                toppopup
-                    .show()
-                    .animate({top:"0",left:"0"}, 500,function(){
-				 var _hide = function hide(){
-                                     toppopup
-                                         .show()
-                                         .animate({top:"-70",left:"0"}, 
-						  500,function(){
-                                                      toppopup.hide();
-                                                      $.cookie('pc_promo_shown', 1,
-							       {domain:'.buildpc.ru', 
-								path:'/', 
-								expires:1000});
-                                                  });
-                                 };
-				 toppopup.find('.small_reset').click(_hide);
-                                 _.delay(_hide, 10000);
-                             });
-            },7000);
-}
-showPromo();
 
 function lockUnlock(){
     if (!document.location.href.match('/computer/')) return;
