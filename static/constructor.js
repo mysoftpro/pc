75f1b79f9386d2f97d2571d8dd7fb9414fb3752e
aganzha <>
2012-02-18 20:18:47 +0300

    consructor

--- static/constructor.js
+++ static/constructor.js
@@ -1,24 +1,137 @@
 var Component = Backbone
     .Model
     .extend({
-
+                initialize:function(){		    
+                    this.set('doc', this.getObjectFromStorage());
+                },
+                getObjectFromStorage:function(){
+                    var _id = this.id;
+                    var storage = this.get('storage');
+		    if (storage['rows'])
+			return _(storage['rows']).chain()
+			.select(function(ob){
+                                    return ob.doc._id == _id;
+                                })
+			.first()
+			.value()
+			.doc;
+		    return _(this.get('storage'))
+                        .chain()
+                        .values()
+                        .map(function(arr){return arr[1];})
+                        .map(function(ob){
+				 return ob[1]['rows'];
+			     })
+                        .map(function(arr){
+                                    return _(arr).select(function(ob){
+                                                             return ob.doc._id == _id;
+                                                         });
+                              })
+                        .select(function(arr){return arr.length>0;})
+                        .first()
+                        .first()
+                        .value()
+			.doc;
+                },
+		dget:function(ob,key,_def){
+		    var def = {};
+		    if (_def)
+			def = _def;
+		    if (ob[key])
+			return ob[key];
+		    return def;
+		},
+                getIcon:function(){
+		    var img = this.dget(this.dget(this.get('doc'),'description'),'imgs',[])[0];
+		    if (img){
+			var ext = '.jpg';
+			if (img.match('jpeg')){
+			    ext = '';
+			    img = encodeURIComponent(path);
+			}
+		    }
+		    return '/image/'+this.id+'/'+img+ext;
+                }
             });
+var SateliteView = Backbone
+    .View
+    .extend({
+                box_size:88,
+                renderCycle:function(){
+                    var context = this.el.getContext("2d");
+                    var centerX = this.box_size / 2;
+                    var centerY = this.box_size  / 2;
+                    var radius = (this.box_size-3)/2;
+
+                    context.beginPath();
+                    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+
+                    context.lineWidth = 5;
+                    context.strokeStyle = "#B9D3E1";
+                    context.stroke();
+                    context.clip();
+
+                    var imageObj = new Image();
+
+                    imageObj.onload = function(){
+
+                        var jimg = $(imageObj);
+                        jimg.css('width','100px');
+                        context.drawImage(imageObj, 0, 0, 235,235);
+                    };
+                    imageObj.src = this.model.getIcon();
+
+                },
+                render:function(){
+                    this.renderCycle();
+                }
+            });
+
+var CentralView = SateliteView
+    .extend({
+                box_size:235
+            });
+
 var Model = Backbone
     .Collection
     .extend({
-                model: Component
+                model: Component,
+                getByPart:function(part){
+                    return this
+                        .chain()
+                        .select(function(c){return c.get('part')==part;})
+                        .first()
+                        .value();
+                },
+                getByAlias:function(alias){
+                    return this
+                        .chain()
+                        .select(function(c){return c.get('alias')==alias;})
+                        .first()
+                        .value();
+                }
             });
 
-var ModelView = Backbone.View.extend({
-  events: {
-  },
-  render: function() {
-  }
-});
+var ModelView = Backbone
+    .View
+    .extend({
+                events: {
+                },
+                getCentralCoords:function(){
+
+                },
+                render: function() {
+                    var _case = this.collection.getByAlias('case');
+                    var case_view = new CentralView({
+                                                    model:_case,
+                                                    el:document.getElementById('ccenter')
+                                                });
+                    console.log(1);
+                    case_view.render();
+                }
+            });
 
 
-var central_box_size = 235;
-var satelite_box_size = 88;
 (function init(){
      var aliases_reverted = {};
      _(parts_aliases).chain().keys().each(function(key){aliases_reverted[parts_aliases[key]]=key;});
@@ -36,14 +149,20 @@ var satelite_box_size = 88;
                   }
                   return new Component({
                                            id:component_id,
-                                           count:count,
-                                           alias:aliases_reverted[key],
                                            part:key,
+                                           alias:aliases_reverted[key],
+                                           count:count,
                                            storage:choices[key]
                                        });
               })
          .value();
      var model_collection = new Model(model_components);
+     var model_view = new ModelView({
+                                        collection:model_collection,
+                                        el:document.getElementById('ccontainer')
+                                    });
+     model_view.render();
+
 
      // var model = new Model()
      // var container = $('#ccontainer');
