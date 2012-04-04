33e159db68758555e318ec7157951ce8b7163604
aganzha <aganzha@aganzha-asus>
2012-04-04 08:43:01 +0300

    fix notes

--- static/notes.js
+++ static/notes.js
@@ -97,19 +97,11 @@ function getHash(){
 }
 
 function init(){
-    var chipvendors = $('.chipVendors li');
+    var chipvendors = $('.chipVendors button');
     chipvendors.click(function(e){
                           var target = $(e.target);
-                          var tag = target[0].tagName.toLowerCase();
-                          if(tag=='a')
-                              return;
-                          var guard = 10;
-                          while(tag!=='li'){
-                              target = target.parent();
-                              tag = target[0].tagName.toLowerCase();
-                          }
                           showComponent({preventDefault:function(){},
-                                         target:{id:'_'+target[0].id}});
+                                         target:{id:'_'+target.parent().attr('id')}});
                       });
     var chipnames = _($('.chipname').toArray()).chain().map(function(el){return $(el);});
     var fltrs = $('.proc_filter')
