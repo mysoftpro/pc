3c90b9f9656d65b75ea4535eb31d783c1cbd3055
aganzha <aganzha@aganzha-asus>
2012-04-24 09:35:22 +0300

    styles

--- used/json/by_date.map.js
+++ used/json/by_date.map.js
@@ -1,6 +1,6 @@
 // && doc.date[2]=='14'
 function(doc){
-    if (doc.date[0]=='2012' && doc.date[1]=='04' && doc.date[2]=='21')
+    if (doc.date[0]=='2012' && doc.date[1]=='04' && doc.date[2]=='23')
        {
 	emit(doc['external_id'],doc['_rev']);
     }
